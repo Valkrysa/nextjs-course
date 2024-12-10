403180716fc849d5dce1d84ac1df87eb989bca04
Alice (raini) Robinson <raini.robinson@doubledown.com>
2024-12-09 16:04:54 -0800

    css react style

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,7 +1,12 @@
 import Post from './components/Post'
 
 function App() {
-    return <Post />;
+    return (
+        <main>
+            <Post author="Alice" body="React.js is awesome!" />
+            <Post author="Valkrysa" body="Check out the full course!" />
+        </main>
+    );
 }
 
 export default App;
--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -1,8 +1,10 @@
-function Post() {
+import classes from './Post.module.css';
+
+function Post(props) {
     return (
-        <div>
-            <p>Alice</p>
-            <p>React.js is awesome!</p>
+        <div className={classes.post}>
+            <p className={classes.author}>{props.author}</p>
+            <p className={classes.text}>{props.body}</p>
         </div>
     );
 }
